a76ea5d81634fbc1a9ccd28e82a9d2f4f6310465
Patrick Doyle <patrickdoyle30@gmail.com>
2019-06-26 14:02:00 -0700

    Initial Commit

---
+++ WeirdFace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeirdFace
+//
+//  Created by Patrick Doyle on 6/26/19.
+//  Copyright © 2019 Patrick Doyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeirdFaceTests/WeirdFaceTests.swift
@@ -0,0 +1,34 @@
+//
+//  WeirdFaceTests.swift
+//  WeirdFaceTests
+//
+//  Created by Patrick Doyle on 6/26/19.
+//  Copyright © 2019 Patrick Doyle. All rights reserved.
+//
+
+import XCTest
+@testable import WeirdFace
+
+class WeirdFaceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
